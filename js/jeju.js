9a76e709b6bd23b4be40679e9a9b2cc62d742b65
jcy981215 <jcy981215@naver.com>
2023-08-31 01:22:14 +0900

    강릉,제주 페이지 완성

---
+++ js/jeju.js
@@ -0,0 +1,105 @@
+gsap.fromTo('.content2 .info > p',{ 
+    scrollTrigger:'.content2',
+    opacity:0,
+    x:0 ,y:100,   
+},{
+    opacity:1,
+    duration:3,
+    x:0 ,y:-50,
+})
+gsap.fromTo('.content2 .info h1',{ 
+    scrollTrigger:'.content2',
+    opacity:0,
+    x:0 ,y:150,   
+},{
+    opacity:1,
+    duration:4,
+    x:0 ,y:-50,
+})
+
+gsap.fromTo('.content2 .info .txt',{ 
+    scrollTrigger:'.content2',
+    opacity:0,
+    x:0 ,y:250,   
+},{
+    opacity:1,
+    duration:5,
+    x:0 ,y:-50,
+})
+gsap.fromTo('.content2 .info > a',{ 
+    scrollTrigger:'.content2',
+    opacity:0,
+    x:0 ,y:250,   
+},{
+    opacity:1,
+    duration:10,
+    x:0 ,y:-50,
+})
+
+gsap.fromTo('.content3 .imgs ul',{ 
+    scrollTrigger:'.content3',
+    opacity:0,
+    x:0 ,y:250,   
+},{
+    opacity:1,
+    duration:8,
+    x:0 ,y:-50,
+})
+
+gsap.fromTo('.content4 table',{ 
+    scrollTrigger:'.content4',
+    opacity:0,
+    scale:0.5,
+},{
+    opacity:1,
+    duration:2,
+    x:0 ,y:-50,
+    scale:1,
+})
+gsap.fromTo('.content4 > p',{ 
+    scrollTrigger:'.content4',
+    opacity:0,
+    x:0 ,y:250,   
+},{
+    opacity:1,
+    duration:4,
+    x:0 ,y:-50,
+})
+
+gsap.fromTo('.content5 p',{ 
+    scrollTrigger:'.content5',
+    opacity:0,
+    x:0 ,y:250,   
+},{
+    opacity:1,
+    duration:2,
+    x:0 ,y:-50,
+})
+gsap.fromTo('.content5 .map',{ 
+    scrollTrigger:'.content5',
+    opacity:0,
+    x:0 ,y:250,   
+},{
+    opacity:1,
+    duration:2.8,
+    x:0 ,y:-50,
+})
+
+gsap.fromTo('.content6 > p',{ 
+    scrollTrigger:'.content6',
+    opacity:0,
+    x:0 ,y:250,   
+},{
+    opacity:1,
+    duration:2,
+    x:0 ,y:-50,
+})
+gsap.fromTo('.content6 ul',{ 
+    scrollTrigger:'.content6',
+    opacity:0,
+    x:0 ,y:250,   
+},{
+    opacity:1,
+    duration:2.8,
+    x:0 ,y:-50,
+})
