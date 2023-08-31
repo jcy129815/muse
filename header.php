0f4ee9bb954cf961a16f17230b9a2f51846e3a3b
jcy <jcy981215@naver.com>
2023-08-31 15:33:41 +0900

    스토리 완성 및 파일 정리

---
+++ header.php
@@ -0,0 +1,20 @@
+
+<header>
+        <div class="header">
+            <h1><a href="./index.html"><img src="./img/muse.png" alt=""></a></h1>
+            <nav>
+                <ul>
+                    <li><a href="./index.html">홈</a></li>
+                    <li><a href="./jeju.html">제주</a></li>
+                    <li><a href="./ganglng.html">강릉</a></li>
+                    <li><a href="./story.html">그대, 나의 뮤즈STORE</a></li>
+                    <li><a href="#">전시소개</a></li>
+                </ul>
+            </nav>
+            <select name="spicking" id="spicking">
+                <option value="KOR">KOR</option>
+                <option value="KOR">EN</option>
+                <option value="KOR">CH</option>
+            </select>
+        </div>
+    </header>
