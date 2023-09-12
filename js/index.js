087d8f78b1ec656f055dab9700bf7c7937dcfbb1
jcy <jcy981215@naver.com>
2023-09-12 17:06:14 +0900

    이미지 수정

--- js/index.js
+++ js/index.js
@@ -47,9 +47,11 @@ const off = document.querySelector('header .header .hidden_nav .hidden a:first-c
 const on = document.querySelector('header .header .hidden_nav > a')
 console.log(hid,on,off)
 hid.style.display = 'none'
-on.addEventListener('click',function(){
+on.addEventListener('click',function(e){
+    e.preventDefault(),
     hid.style.display = 'flex'
 })
-off.addEventListener('click',function(){
+off.addEventListener('click',function(e){
+    e.preventDefault(),
     hid.style.display = 'none'
 })
