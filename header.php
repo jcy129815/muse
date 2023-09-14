28c3514e3b5e570ff9706939d7aec43d13e0b223
jcy <jcy981215@naver.com>
2023-09-14 12:58:19 +0900

    header/php 수정

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
-
-<header>
+    
+    <header>
         <div class="header">
             <h1><a href="./index.html"><img src="./img/muse.png" alt=""></a></h1>
             <nav>
@@ -16,5 +16,16 @@
                 <option value="KOR">EN</option>
                 <option value="KOR">CH</option>
             </select>
+            <div class="hidden_nav">
+                <a href="#"><img src="./img/menuon.png" alt=""></a>
+                <div class="hidden">
+                    <a href="#"><img src="./img/menuoff.png" alt=""></a>
+                    <a href="./index.html">홈</a>
+                    <a href="./jeju.html">제주</a>
+                    <a href="./ganglng.html">강릉</a>
+                    <a href="./story.html">그대, 나의 뮤즈STORE</a>
+                    <a href="#">공지사항</a>
+                </div>
+            </div>
         </div>
     </header>
