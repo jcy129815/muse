b725e2f964d116e8a4effaf0af096324feab35e5
jcy <jcy981215@naver.com>
2023-09-15 16:18:33 +0900

    js뭐지

--- js/story.js
+++ js/story.js
@@ -2,18 +2,19 @@ const btn1 = document.querySelector('.topmenu > div > #event')
 const btn2 = document.querySelector('.topmenu > div > #str')
 const eve = document.querySelector('.eve')
 const story = document.querySelector('.story')
+console.log(btn1,btn2,eve,story)
 story.style.display = 'none'
 btn2.addEventListener('click',function(e){
     e.preventDefault(),
-    story.style.display = 'block'
-    eve.style.display = 'none'
-    btn2.style.color = '#7A0000'
+    story.style.display = 'block',
+    eve.style.display = 'none',
+    btn2.style.color = '#7A0000',
     btn1.style.color = '#fff'
 })
 btn1.addEventListener('click',function(e){
     e.preventDefault(),
-    story.style.display = 'none'
-    eve.style.display = 'block'
-    btn1.style.color = '#023E98'
+    story.style.display = 'none',
+    eve.style.display = 'block',
+    btn1.style.color = '#023E98',
     btn2.style.color = '#fff'
 })
